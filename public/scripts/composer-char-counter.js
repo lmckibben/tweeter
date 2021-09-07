36b7ea6fcf0afd4acbb145d26163af04aa4ec52e
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-07 21:43:20 +0000

    added logic to change color to red if charLimit is below 0, and change it back once its more

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
   $tweetText.on("keydown", function() {
     const textLength = this.textLength;
     let charLimit = 139 - textLength;
-
-    console.log(charLimit);
+    if (charLimit < 0) {
+      $counter.css('color', 'red');
+    }
+    if (charLimit >= 0) {
+      $counter.css('color', '#545149');
+    }
     $counter.text(charLimit)
   });
 });
