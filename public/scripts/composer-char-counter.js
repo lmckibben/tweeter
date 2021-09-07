896c6f530047c7b3bb60612c8fbae99c482beaf1
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-07 21:44:59 +0000

    added logic to keep track of backspaces and delete to add 2 when key is pressed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   $tweetText.on("keydown", function() {
     const textLength = this.textLength;
     let charLimit = 139 - textLength;
+    const key = event.keyCode || event.charCode;
+    if( key == 8 || key == 46 ) {
+      charLimit += 2
+    }
     if (charLimit < 0) {
       $counter.css('color', 'red');
     }
