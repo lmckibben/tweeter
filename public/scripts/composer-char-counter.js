8bec626958af8a8d51c849c1a237ebca1eae3af0
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-07 21:41:38 +0000

    setting text inside counter to change based on text length-charLimit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   const $tweetText = $('#tweet-text');
   const $counter = $(".counter");
   $tweetText.on("keydown", function() {
-    
+    const textLength = this.textLength;
+    let charLimit = 139 - textLength;
+
+    console.log(charLimit);
+    $counter.text(charLimit)
   });
 });
