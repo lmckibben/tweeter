bea6e59fdda6432804df72bf615088f3b6b3f271
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-08 19:20:26 +0000

    added in tweetData obj, created function or create tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,22 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+$(() => {
+  
+  const createTweetElement = function(obj) {
+    
+  }
+
+});
