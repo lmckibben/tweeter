1ef701b574b1ce68d2c78a986f49e7b3f8781b79
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-08 19:47:23 +0000

    set createTweetElement to output html of tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,9 +17,35 @@ const tweetData = {
 }
 
 $(() => {
-  
+
   const createTweetElement = function(obj) {
-    
+    const tweetContainer = `
+    <section class="tweet-container">
+      <header class="profile">
+      <article class="profile-container">
+        <img src="${obj.user.avatars}">
+        <p class="profile-name">${obj.user.name}</p>
+      </article>
+      <article class="user-handle">${obj.user.handle}</article>
+    </header>
+    <p class="tweet-content">${obj.content.text}</p>
+    <footer class="tweet-footer">
+      <article class="tweet-icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </article>
+      <p class="days-since">${obj.created_at}</p>
+    </footer>
+  </section>
+    `
+    return tweetContainer
   }
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
