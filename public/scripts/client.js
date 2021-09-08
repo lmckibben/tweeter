7a700bf21028f95db27091fd7e5390543aa499ac
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-08 20:37:22 +0000

    added an event listener, stopped default behaviour for submit, serializedTweet and posted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
+const tweetData = [{
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -14,9 +14,19 @@ const tweetData = {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
   "created_at": 1461116232227
-}
+}]
 
 $(() => {
+
+  const renderTweets = function(tweets) {
+    let output = "";
+
+    for (const tweet of tweets) {
+      output += createTweetElement(tweet);
+    }
+
+    return output;
+  };
 
   const createTweetElement = function(obj) {
     const tweetContainer = `
@@ -42,10 +52,18 @@ $(() => {
     return tweetContainer
   }
 
-const $tweet = createTweetElement(tweetData);
+  const $form = $('#new-tweet-form');
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    const serializedTweet = $(this).serialize();
+    console.log(serializedTweet);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    $.post('/tweets', serializedTweet, (response) => {
+      console.log(response)
+      
+    });
+  })
 
+
+$('#tweets-container').prepend(renderTweets(tweetData));
 });
